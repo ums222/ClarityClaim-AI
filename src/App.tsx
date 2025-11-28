cba537619b36484923955a44938b289ef2ce32b8
Umidjon Saidkhujaev <147444629+ums222@users.noreply.github.com>
2025-11-28 01:31:17 -0500

    Wrap App with ThemeProvider for theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import { ThemeProvider } from "./hooks/useTheme";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      {/* Future routes */}
-      {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
-    </Routes>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* Future routes */}
+        {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
+      </Routes>
+    </ThemeProvider>
   );
 }
 
